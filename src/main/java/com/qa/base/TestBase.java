a2ea92066259c7984e26885cb78ac32b883926e7
harikrishna8554 <hanishkrishna27@gmail.com>
2019-12-26 12:07:23 +0530

    contacts page added

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -10,5 +10,11 @@ public class TestBase {
 	{
 		
 	}
+	
+	@Test
+	public void modifiedforGit()
+	{
+		
+	}
 
 }
---
+++ src/main/java/com/qa/pages/ContactsPage.java
@@ -0,0 +1,14 @@
+package com.qa.pages;
+
+import org.testng.annotations.Test;
+
+public class ContactsPage {
+
+	@Test
+	public void contactsTitleTest()
+	{
+		
+		
+		
+	}
+}
